27570ce054a36100208afcb42793dc1ba614f5be
Isaac Christensen <ichristensen@lucidchart.com>
2023-12-06 13:46:24 -0700

    Working CompInterface

--- src/scmb.cpp
+++ src/scmb.cpp
@@ -1,11 +1,18 @@
 #include "usbPackets.hpp"
 #include "bufferedUsb.hpp"
+#include <iostream>
+#include <iomanip>
 
 UsbChannel *channel;
 
 bool UsbReadWordRequest::actOnPkt()
 {
-    channel->SendPacket(new UsbReadWordResponse(this->Address.data, this->Address.data * 2, this->PktCnt.data));
+    uint32_t addressData = this->Address.data;
+    uint32_t response = addressData * 2;
+    std::cout << "Got a request for mem 0x" << std::hex << std::setw(16) << std::setfill('0') << addressData
+              << " responded with 0x" << std::hex << std::setw(16) << std::setfill('0') << response << std::dec << std::endl;
+    // printf("Got a request for mem %#018x responded with %#010x", this->Address.data, response);
+    channel->SendPacket(new UsbReadWordResponse(addressData, response, this->PktCnt.data));
     return true;
 }
 bool UsbReadWordResponse::actOnPkt()
@@ -16,10 +23,11 @@ bool UsbReadWordResponse::actOnPkt()
 int main()
 {
 
-    channel = new UsbChannel(new BufferedUsb("ttyS3"));
+    channel = new UsbChannel(new BufferedUsb("/dev/ttyACM0"));
 
     while (1)
     {
+        channel->channel->asyncHandler();
         if (channel->PacketReady())
         {
             channel->processNextPacket();
