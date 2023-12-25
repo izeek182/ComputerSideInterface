cb44c73016efd1c83129d220b20136ba65faa227
Isaac Christensen <ichristensen@lucidchart.com>
2023-12-25 13:48:07 -0700

    Oops I guess this was not in the archive :shrug:

--- build/scmb.d
+++ build/scmb.d
@@ -1,1 +1,32 @@
-build/scmb.o: src/scmb.cpp
+build/scmb.o: src/scmb.cpp Common-Lib/Comms/Inc/usbPackets.hpp \
+ Common-Lib/Comms/Inc/uartData.hpp Common-Lib/Core/Inc/iQueue.hpp \
+ Common-Lib/Core/Inc/zHal.h Common-Lib/Comms/Inc/uartPacket.hpp \
+ Common-Lib/Core/Inc/zHal.h Common-Lib/Comms/Inc/iBufferedChannel.hpp \
+ Common-Lib/Core/Inc/ringCharBuffer.hpp Common-Lib/Core/Inc/iQueue.hpp \
+ Common-Lib/Usb/Inc/bufferedUsb.hpp \
+ Common-Lib/Comms/Inc/iBufferedChannel.hpp \
+ Common-Lib/Usb/Inc/globalMem.hpp
+
+Common-Lib/Comms/Inc/usbPackets.hpp:
+
+Common-Lib/Comms/Inc/uartData.hpp:
+
+Common-Lib/Core/Inc/iQueue.hpp:
+
+Common-Lib/Core/Inc/zHal.h:
+
+Common-Lib/Comms/Inc/uartPacket.hpp:
+
+Common-Lib/Core/Inc/zHal.h:
+
+Common-Lib/Comms/Inc/iBufferedChannel.hpp:
+
+Common-Lib/Core/Inc/ringCharBuffer.hpp:
+
+Common-Lib/Core/Inc/iQueue.hpp:
+
+Common-Lib/Usb/Inc/bufferedUsb.hpp:
+
+Common-Lib/Comms/Inc/iBufferedChannel.hpp:
+
+Common-Lib/Usb/Inc/globalMem.hpp:
--- src/scmb.cpp
+++ src/scmb.cpp
@@ -2,17 +2,42 @@
 #include "bufferedUsb.hpp"
 #include <iostream>
 #include <iomanip>
+#include "globalMem.hpp"
 
 UsbChannel *channel;
+Memory *mem;
+
+bool UsbWriteWordRequest::actOnPkt()
+{
+    uint32_t addressData = this->Address.data;
+    uint32_t Data = this->Value.data;
+    mem->writeMemory(addressData, Data);
+    std::cout << "Request #" << std::hex << std::setw(4) << std::setfill('0') << this->PktCnt.data
+              << " request: write  data to mem 0x" << std::hex << std::setw(8) << std::setfill('0') << addressData
+              << " Writing data with 0x" << std::hex << std::setw(8) << std::setfill('0') << Data << std::dec << std::endl;
+    // printf("Got a request for mem %#018x responded with %#010x", this->Address.data, response);
+    UsbPacket *pkt = new UsbWriteWordResponse(addressData, this->PktCnt.data);
+    channel->SendPacket(pkt);
+    delete pkt;
+    return true;
+}
+
+bool UsbWriteWordResponse::actOnPkt()
+{
+    return false;
+}
 
 bool UsbReadWordRequest::actOnPkt()
 {
     uint32_t addressData = this->Address.data;
-    uint32_t response = addressData * 2;
-    std::cout << "Got a request for mem 0x" << std::hex << std::setw(16) << std::setfill('0') << addressData
-              << " responded with 0x" << std::hex << std::setw(16) << std::setfill('0') << response << std::dec << std::endl;
+    uint32_t response = mem->readMemory(addressData);
+    std::cout << "Request #" << std::hex << std::setw(4) << std::setfill('0') << this->PktCnt.data
+              << " request: read data from mem 0x" << std::hex << std::setw(8) << std::setfill('0') << addressData
+              << " responded with 0x" << std::hex << std::setw(8) << std::setfill('0') << response << std::dec << std::endl;
     // printf("Got a request for mem %#018x responded with %#010x", this->Address.data, response);
-    channel->SendPacket(new UsbReadWordResponse(addressData, response, this->PktCnt.data));
+    UsbPacket *pkt = new UsbReadWordResponse(addressData, response, this->PktCnt.data);
+    channel->SendPacket(pkt);
+    delete pkt;
     return true;
 }
 bool UsbReadWordResponse::actOnPkt()
@@ -20,10 +45,17 @@ bool UsbReadWordResponse::actOnPkt()
     return false;
 }
 
-int main()
+int main(int argc, char *argv[])
 {
+    if (argc < 3)
+    {
+        std::cerr << "Usage: " << argv[0] << " [interface name] [bin file path]" << std::endl;
+        return 1;
+    }
 
-    channel = new UsbChannel(new BufferedUsb("/dev/ttyACM0"));
+    channel = new UsbChannel(new BufferedUsb(argv[1]));
+    mem = new Memory();
+    mem->loadBinFile(argv[2]);
 
     while (1)
     {
